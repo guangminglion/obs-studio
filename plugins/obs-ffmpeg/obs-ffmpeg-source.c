cc03e8052f30116d7511a242aca070aa63a3b44d
jp9000 <obs.jim@gmail.com>
2016-01-14 06:02:28 -0800

    fixup! libobs: Add source output flag OBS_SOURCE_DO_NOT_DUPLICATE

--- plugins/obs-ffmpeg/obs-ffmpeg-source.c
+++ plugins/obs-ffmpeg/obs-ffmpeg-source.c
@@ -551,7 +551,8 @@ static void ffmpeg_source_destroy(void *data)
 struct obs_source_info ffmpeg_source = {
 	.id             = "ffmpeg_source",
 	.type           = OBS_SOURCE_TYPE_INPUT,
-	.output_flags   = OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_AUDIO,
+	.output_flags   = OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_AUDIO |
+	                  OBS_SOURCE_DO_NOT_DUPLICATE,
 	.get_name       = ffmpeg_source_getname,
 	.create         = ffmpeg_source_create,
 	.destroy        = ffmpeg_source_destroy,
